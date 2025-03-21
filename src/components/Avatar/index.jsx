4213a53c636d591e67d9ef17905a80776d1c3d76
NguyenXuanVinh <nongami87@gmail.com>
2025-03-21 22:32:55 +0700

    feat: add avatar component with styles

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import validateProps from "@/utils/validateProps";
+import styles from "./Avatar.module.css";
+
+function Avatar({ name, thumbnail, outline = false, status = false }) {
+  validateProps(Avatar, { name, thumbnail, outline, status });
+
+  return (
+    <div className={styles.avatar}>
+      <img src={thumbnail} alt={name} />
+      {status && <div></div>}
+    </div>
+  );
+}
+
+Avatar.propTypes = {
+  name: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  outline: PropTypes.bool,
+  status: PropTypes.bool,
+};
+
+export default Avatar;

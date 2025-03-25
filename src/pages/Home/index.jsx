107806e0784408032c3f147115c12d0c047fe9cf
NguyenXuanVinh <nongami87@gmail.com>
2025-03-25 09:36:30 +0700

    feat: add logout button and functionality

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
+import config from "@/config";
+import auth from "../Auth/auth";
 import SideBar from "./SideBar";
 import FriendsList from "./FriendsList";
 import styles from "./Home.module.css";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    const data = auth("logout", null, {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    });
+
+    if (data) {
+      localStorage.removeItem("token");
+      navigate(config.routes.login);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <aside className={styles.directMessages}>
@@ -11,7 +28,11 @@ function Home() {
       <main className={styles.main}>
         <FriendsList />
       </main>
-      <aside className={styles.activeUsers}></aside>
+      <aside className={styles.activeUsers}>
+        <button className={styles.logoutBtn} onClick={handleLogout}>
+          Log Out
+        </button>
+      </aside>
     </div>
   );
 }

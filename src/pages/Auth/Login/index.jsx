59713ed751513009695eeafea38e4e08781ad8f4
NguyenXuanVinh <nongami87@gmail.com>
2025-04-01 20:41:36 +0700

    feat: implement login form with validation schema

--- src/pages/Auth/Login/index.jsx
+++ src/pages/Auth/Login/index.jsx
@@ -1,78 +1,81 @@
 import { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import config from "@/config";
 import httpRequest from "@/utils/httpRequest";
 import authService from "@/services/authService";
+import loginSchema from "@/schema/loginSchema";
 
 import Button from "@/components/Button";
-import styles from "../Auth.module.css";
 import ErrorMessage from "../ErrorMessage";
+import styles from "../Auth.module.css";
 
 function Login() {
-  const [params] = useSearchParams();
   const navigate = useNavigate();
+  const [params] = useSearchParams();
 
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(loginSchema),
+  });
 
-  const errorMessages = hasError ? "Login or password is invalid." : "";
-  const ErrorComponent = <ErrorMessage errorMessage={errorMessages} />;
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const onSubmit = async (loginInfo) => {
     setIsLoading(true);
 
-    const formValues = { email: login, password };
-    const data = await authService.login(formValues);
+    const data = await authService.login(loginInfo);
 
     if (data.status === "success") {
       httpRequest.setToken(data.access_token);
       navigate(params.get("continue") || config.routes.home);
     } else {
-      setHasError(true);
+      const message = "Login or password is invalid.";
+      setError("email", { message });
+      setError("password", { message });
     }
 
     setIsLoading(false);
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <div className={styles.header}>
         <h1>Welcome back!</h1>
         <p>We're so excited to see you again</p>
       </div>
 
       <div className={`${styles.fieldWrapper} ${styles.marginBottom20}`}>
-        <label htmlFor="email" className={hasError ? styles.error : ""}>
-          Email or phone number {ErrorComponent}
+        <label htmlFor="email" className={errors.email ? styles.error : ""}>
+          Email or phone number{" "}
+          <ErrorMessage errorMessage={errors.email?.message} />
         </label>
         <input
           type="text"
           id="email"
-          name="email"
-          required
+          {...register("email")}
           autoComplete="on"
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
         />
       </div>
 
       <div className={styles.fieldWrapper}>
-        <label htmlFor="password" className={hasError ? styles.error : ""}>
-          Password {ErrorComponent}
+        <label
+          htmlFor="password"
+          className={errors.password ? styles.error : ""}
+        >
+          Password <ErrorMessage errorMessage={errors.password?.message} />
         </label>
         <input
           type="password"
           id="password"
-          name="password"
-          required
+          {...register("password")}
           autoComplete="on"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
 

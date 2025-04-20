8949d7a9b18fb97e06a00087eb92f425ee962f28
NguyenXuanVinh <nongami87@gmail.com>
2025-04-20 16:10:44 +0700

    fix: set initial loading state to false

--- src/contexts/LoadingContext.jsx
+++ src/contexts/LoadingContext.jsx
@@ -4,7 +4,7 @@ import LoadingScreen from "@/components/LoadingScreen";
 const LoadingContext = createContext();
 
 function LoadingProvider({ children }) {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const value = {
     loading,
@@ -13,7 +13,6 @@ function LoadingProvider({ children }) {
 
   return (
     <LoadingContext.Provider value={value}>
-      {/* {loading ? <LoadingScreen /> : children} */}
       {loading && <LoadingScreen />}
       {children}
     </LoadingContext.Provider>

5eadf8a194761191b216688cc2215a77010c8abf
NguyenXuanVinh <nongami87@gmail.com>
2025-05-31 21:04:31 +0700

    feat: initialize Next.js app with Tailwind and ESLint

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Discord",
+  description: "A clone of Discord",
+  openGraph: {
+    title: "Discord",
+    description: "A clone of Discord",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
